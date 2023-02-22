c24cf19d5d3a0b4d9c56b075417189e4a52cc7b0
jtrilakshmiv <jhanu1509@gmail.com>
2023-02-22 15:13:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val items = fetchData()
+        val adaptor = RVAdaptor(items)
+        recyclerView.adapter = adaptor
+    }
+
+    fun fetchData(): ArrayList<String> {
+        val list = ArrayList<String>()
+        for (i in 0 until 20) {
+            list.add("Item $i")
+        }
+        return list
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/RVAdaptor.kt
@@ -0,0 +1,26 @@
+package com.example.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RVAdaptor(private val items:ArrayList<String>) : RecyclerView.Adapter<RVAdaptor.ViewHolder>() {
+    class ViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView) {
+        val textView : TextView = itemView.findViewById(androidx.recyclerview.R.id.text)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.custom_design,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int {
+       return items.size
+    }
+}
